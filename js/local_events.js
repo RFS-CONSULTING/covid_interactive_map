21fb06199cebdaedaacd6f09d953dfb6bb450869
seleshabani <seleshabani4@gmail.com>
2022-02-11 03:16:41 +0100

    fix css and show more data

--- js/local_events.js
+++ js/local_events.js
@@ -1,12 +1,14 @@
 var sitcovid_nom = document.querySelector('#input_sitcovid_nom')
 var sitcovid_superf = document.querySelector('#input_sitcovid_superf')
 var sitcovid_newsc = document.querySelector('#input_sitcovid_newcas')
+var sitcovid_oldc = document.querySelector('#input_sitcovid_oldcas')
 
 var activites_vaccin_local_event = function(e){
     var target = e.target;
-    console.log(target)
+    // console.log(e.target.feature.properties)
     // sitcovid_nom.value = e.target.feature.properties.NOM
     sitcovid_nom.value = e.target.feature.properties.NOM
-    sitcovid_superf.value = e.target.feature.properties.SURFACE
-    sitcovid_newsc.value = e.target.feature.properties['Joint_Nouveau Cas Covid-19 (30/12/2021)']
+    sitcovid_superf.value = e.target.feature.properties.SURFACE + ' Km²'
+    sitcovid_newsc.value = e.target.feature.properties['Joint_Nouveau Cas Covid-19 (30/12/2021)']?e.target.feature.properties['Joint_Nouveau Cas Covid-19 (30/12/2021)']:'0'
+    sitcovid_oldc.value = e.target.feature.properties['Joint_Nbre de Cas Covid-19']
 }
